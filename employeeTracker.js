5ae06217e4bf268394351a01ce5dc139d2197a0d
lauren-briggs <laurenbriggs41@gmail.com>
2021-05-19 22:34:16 +1000

    added first askQuestion function and start of addToTable functionaltiy

--- employeeTracker.js
+++ employeeTracker.js
@@ -10,18 +10,140 @@ const connection = mysql.createConnection({
     database: 'employeeDB',
 });
 
-const readEmployeeRole = () => {
-    connection.query(
-        'SELECT role.id, role.title, employee.first_name, employee.last_name, employee.role_id FROM role INNER JOIN employee ON role.id=employee.role_id;',
-        (err, res) => {
-            if (err) throw err;
-            console.table(res);
-            connection.end();
+
+const askQuestion = () => {
+    inquirer.prompt({
+        name: 'action',
+        type: 'list',
+        message: 'What would you like to do?',
+        choices: [
+            'Add to a table',
+            'View a table',
+            'Update an employee',
+            'Exit',
+        ]
+    })
+        .then((answer) => {
+            switch (answer.action) {
+                case 'Add to a table':
+                    addToTable();
+                    break;
+                case 'View a table':
+                    viewTable();
+                    break;
+                case 'Update an employee':
+                    updateEmployee();
+                    break;
+                case 'Exit':
+                    connection.end();
+                    break;
+                default:
+                    console.log(`Invalid action ${answer.action}`);
+                    break;
+            }
         })
 }
+
+const addToTable = () => {
+    inquirer.prompt(
+        {
+            name: 'addTo',
+            type: 'list',
+            message: 'Which table would you like to add to?',
+            choices: [
+                'department',
+                'role',
+                'employee',
+            ]
+        })
+        .then((answer) => {
+            switch (answer.addTo) {
+                case 'department':
+                    inquirer.prompt({
+                        name: 'depName',
+                        type: 'input',
+                        message: 'What department would you like to add?',
+                    }).then((departmentData) => {
+                        console.table(departmentData);
+                        return departmentData;
+                    })
+                    break;
+                case 'role':
+                    inquirer.prompt([
+                        {
+                            name: 'roleName',
+                            type: 'input',
+                            message: 'What role would you like to add?',
+                        },
+                        {
+                            name: 'salary',
+                            type: 'input',
+                            message: 'What is the salary?',
+                        }
+                    ]).then((roleData) => {
+                        console.table(roleData);
+                        return roleData;
+                    })
+                    break;
+                case 'employee':
+                    inquirer.prompt([
+                        {
+                            name: 'firstName',
+                            type: 'input',
+                            message: 'What is the employees first name?',
+                        },
+                        {
+                            name: 'lastName',
+                            type: 'input',
+                            message: 'What is their last name?',
+                        },
+                        {
+                            name: 'role',
+                            type: 'list',
+                            message: 'What is their role?',
+                            choices: [
+                                'Web Developer',
+                                'Graphic Designer',
+                                'Digital Designer',
+                                'Creative Director',
+                                'Copywriter',
+                                'Social Media Manager',
+                                'Accountant',
+                                'Manager',
+                            ]
+                        }
+                    ]).then((employeeData) => {
+                        console.table(employeeData);
+                        return employeeData;
+                    })
+                    break;
+                default:
+                    console.log(`Invalid action ${answer.addTo}`);
+                    break;
+            }
+        })
+
+    // .then(askQuestion);
+
+    // connection.query(
+    //     `INSERT INTO ${answer.addTo} SET.....`
+    //  )
+}
+
+
+// const readEmployeeRole = () => {
+//     connection.query(
+//         'SELECT role.id, role.title, employee.first_name, employee.last_name, employee.role_id FROM role INNER JOIN employee ON role.id=employee.role_id;',
+//         (err, res) => {
+//             if (err) throw err;
+//             console.table(res);
+//             connection.end();
+//         })
+// }
 
 connection.connect((err) => {
     if (err) throw err;
     console.log(`Connected as id ${connection.threadId}`);
-    readEmployeeRole();
+    // readEmployeeRole();
+    askQuestion();
 });
--- seeds.sql
+++ seeds.sql
@@ -14,11 +14,11 @@ VALUES
 ('Web Developer', 90000, 2),
 ('Graphic Designer', 70000, 1),
 ('Digital Designer', 75000, 1),
-('Creative Director', 40000, 1);
-('Copywriter', 40000, 3);
-('Social Media Manager', 40000, 3);
-('Accountant', 100000, 4)
-('Manager', 100000, 5)
+('Creative Director', 40000, 1),
+('Copywriter', 40000, 3),
+('Social Media Manager', 40000, 3),
+('Accountant', 100000, 4),
+('Manager', 100000, 5);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES 
@@ -27,5 +27,5 @@ VALUES
 ('Amelia', 'Lucas', 8, 789),
 ('Georgia', 'Wilson', 7, 234),
 ('Isabella', 'James', 2, 567),
-('Carly', 'Fawell', 5, 890);
+('Carly', 'Fawell', 5, 890),
 ('Alice', 'Lane', 4, 012);
