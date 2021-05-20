24fd907f13c0cd20ada98589184165cee0386d90
lauren-briggs <laurenbriggs41@gmail.com>
2021-05-20 20:44:25 +1000

    added viewing table and add to table functionality

--- employeeTracker.js
+++ employeeTracker.js
@@ -17,20 +17,40 @@ const askQuestion = () => {
         type: 'list',
         message: 'What would you like to do?',
         choices: [
-            'Add to a table',
+            'Add a department',
+            'Add a role',
+            'Add an employee',
             'View a table',
+            // 'View departments',
+            // 'View roles',
+            // 'View employees',
             'Update an employee',
             'Exit',
         ]
     })
         .then((answer) => {
             switch (answer.action) {
-                case 'Add to a table':
-                    addToTable();
+                case 'Add a department':
+                    addDepartment();
+                    break;
+                case 'Add a role':
+                    addRole();
+                    break;
+                case 'Add an employee':
+                    addEmployee();
                     break;
                 case 'View a table':
                     viewTable();
                     break;
+                // case 'View departments':
+                //     viewDepartments();
+                //     break;
+                // case 'View roles':
+                //     viewRoles();
+                //     break;
+                // case 'View employees':
+                //     viewEmployees();
+                //     break;
                 case 'Update an employee':
                     updateEmployee();
                     break;
@@ -41,109 +61,114 @@ const askQuestion = () => {
                     console.log(`Invalid action ${answer.action}`);
                     break;
             }
-        })
+        });
 }
 
-const addToTable = () => {
-    inquirer.prompt(
-        {
-            name: 'addTo',
-            type: 'list',
-            message: 'Which table would you like to add to?',
-            choices: [
-                'department',
-                'role',
-                'employee',
-            ]
-        })
-        .then((answer) => {
-            switch (answer.addTo) {
-                case 'department':
-                    inquirer.prompt({
-                        name: 'depName',
-                        type: 'input',
-                        message: 'What department would you like to add?',
-                    }).then((departmentData) => {
-                        console.table(departmentData);
-                        return departmentData;
-                    })
-                    break;
-                case 'role':
-                    inquirer.prompt([
-                        {
-                            name: 'roleName',
-                            type: 'input',
-                            message: 'What role would you like to add?',
-                        },
-                        {
-                            name: 'salary',
-                            type: 'input',
-                            message: 'What is the salary?',
-                        }
-                    ]).then((roleData) => {
-                        console.table(roleData);
-                        return roleData;
-                    })
-                    break;
-                case 'employee':
-                    inquirer.prompt([
-                        {
-                            name: 'firstName',
-                            type: 'input',
-                            message: 'What is the employees first name?',
-                        },
-                        {
-                            name: 'lastName',
-                            type: 'input',
-                            message: 'What is their last name?',
-                        },
-                        {
-                            name: 'role',
-                            type: 'list',
-                            message: 'What is their role?',
-                            choices: [
-                                'Web Developer',
-                                'Graphic Designer',
-                                'Digital Designer',
-                                'Creative Director',
-                                'Copywriter',
-                                'Social Media Manager',
-                                'Accountant',
-                                'Manager',
-                            ]
-                        }
-                    ]).then((employeeData) => {
-                        console.table(employeeData);
-                        return employeeData;
-                    })
-                    break;
-                default:
-                    console.log(`Invalid action ${answer.addTo}`);
-                    break;
+const addDepartment = () => {
+    inquirer.prompt({
+        name: 'name',
+        type: 'input',
+        message: 'What department would you like to add?',
+    }).then((departmentData) => {
+        const query = connection.query(
+            `INSERT INTO department SET ?`, departmentData, (err, res) => {
+                if (err) throw err;
+                console.log(`\nInserting ${departmentData.name} into department...`)
+                console.log(`${res.affectedRows} inserted into department table`);
+                askQuestion();
             }
-        })
-
-    // .then(askQuestion);
-
-    // connection.query(
-    //     `INSERT INTO ${answer.addTo} SET.....`
-    //  )
+        )
+    });
 }
 
+const addRole = () => {
+    inquirer.prompt([
+        {
+            name: 'title',
+            type: 'input',
+            message: 'What role would you like to add?',
+        },
+        {
+            name: 'salary',
+            type: 'input',
+            message: 'What is the salary?',
+        }
+    ]).then((roleData) => {
+        const query = connection.query(
+            `INSERT INTO role SET ?`, roleData, (err, res) => {
+                if (err) throw err;
+                console.log(`\nInserting ${roleData.name} into role...`)
+                console.log(`${res.affectedRows} inserted into role table`);
+                askQuestion();
+            }
+        )
+    });
+}
 
-// const readEmployeeRole = () => {
-//     connection.query(
-//         'SELECT role.id, role.title, employee.first_name, employee.last_name, employee.role_id FROM role INNER JOIN employee ON role.id=employee.role_id;',
-//         (err, res) => {
-//             if (err) throw err;
-//             console.table(res);
-//             connection.end();
-//         })
-// }
+const addEmployee = () => {
+    inquirer.prompt([
+        {
+            name: 'firstname',
+            type: 'input',
+            message: 'What is the employees first name?',
+        },
+        {
+            name: 'lastname',
+            type: 'input',
+            message: 'What is their last name?',
+        },
+        {
+            name: 'role',
+            type: 'list',
+            message: 'What is their role?',
+            choices: [
+                'Web Developer',
+                'Graphic Designer',
+                'Digital Designer',
+                'Creative Director',
+                'Copywriter',
+                'Social Media Manager',
+                'Accountant',
+                'Manager',
+            ]
+        }
+    ]).then((employeeData) => {
+        const query = connection.query(
+            `INSERT INTO employee (first_name, last_name) VALUES ('${employeeData.firstname}', '${employeeData.lastname}')`, (err, res) => {
+                if (err) throw err;
+                console.log(`\nInserting ${employeeData.firstname} ${employeeData.lastname} into employee...`)
+                console.log(`${res.affectedRows} inserted into employee table`);
+                askQuestion();
+            }
+        )
+    });
+}
+
+const viewTable = () => {
+    inquirer.prompt({
+        name: 'table',
+        type: 'list',
+        message: 'Which table would you like to view?',
+        choices: [
+            'department',
+            'role',
+            'employee',
+        ]
+    }).then((table) => {
+        connection.query(
+            `SELECT * FROM ${table.table}`, (err, res) => {
+                if (err) throw err;
+                console.log(`------------\nShowing ${table.table} table\n------------`);
+                console.table(res);
+                askQuestion();
+            }
+        );
+    });
+
+}
 
 connection.connect((err) => {
     if (err) throw err;
     console.log(`Connected as id ${connection.threadId}`);
-    // readEmployeeRole();
     askQuestion();
 });
